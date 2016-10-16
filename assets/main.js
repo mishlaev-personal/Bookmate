97b93e31a34447efc48c9fbe2b2ad3ddc9abd4f0
mishlaev-personal <mishlaev@gmail.com>
2016-10-16 16:12:53 +0300

    Listing and sorting
    Выводятся цитаты и сортируются в хронологическом порядке

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,58 @@
 $(function() {
 
+// Converting timestamp to human date
+  function unixEpochTime_TO_Date_DDMMYY (unixEpochTime, returnUTC) {
+      var year, month, day;
+      var dateObj = new Date (unixEpochTime * 1000);
+
+      if (returnUTC) {
+          year    = dateObj.getUTCFullYear ();
+          month   = dateObj.getUTCMonth ();
+          day     = dateObj.getUTCDate ();
+      }
+      else {
+          year    = dateObj.getFullYear ();
+          month   = dateObj.getMonth ();
+          day     = dateObj.getDate ();
+      }
+
+      //-- Month starts with 0, not 1.  Compensate.
+      month      += 1;
+
+      /*-- Since we want DDMMYY, we need to trim the year and zero-pad
+          the day and month.
+          Note:  Use YYMMDD, for sorting that makes sense.
+      */
+      year    = (""  + year) .slice (-2);
+      month   = ("0" + month).slice (-2);
+      day     = ("0" + day)  .slice (-2);
+
+      return day + '/' + month + '/' + year;
+  };
+
+//Sorting func
+  function custom_sort(a, b) {
+      return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+  }
+
+// Showimg reviews
+
   $.getJSON( "assets/reviews.json", function( data ) {
-    console.log("data");
-    var items = [];
+    console.log(data);
+    var reviews = [];
+
+    console.log(data.sort(custom_sort));
+
     $.each( data, function( key, val ) {
-      items.push( "<li id='" + key + "'>" + val + "</li>" );
+      reviews.push( "<li id='" + key + "'>" + unixEpochTime_TO_Date_DDMMYY(val.created_at, " Local") + ' ' +  val.content + "</li>" );
     });
 
-    $( "<ul/>", {
+    $( "<ol/>", {
       "class": "my-new-list",
-      html: items.join( "" )
+      html: reviews.join( "" )
     }).appendTo( "body" );
   });
+
+
 
 });
