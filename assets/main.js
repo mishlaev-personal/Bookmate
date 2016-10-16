d558a2630e8c020910bd3efaba813bd2e5a014b6
mishlaev-personal <mishlaev@gmail.com>
2016-10-16 15:30:34 +0300

    Initial commit

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  $.getJSON( "assets/reviews.json", function( data ) {
+    console.log("data");
+    var items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+
+    $( "<ul/>", {
+      "class": "my-new-list",
+      html: items.join( "" )
+    }).appendTo( "body" );
+  });
+
+});
